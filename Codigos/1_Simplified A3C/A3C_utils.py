99a77f98a1f7e0f77bcf7b8911c3a231e86d7889
Villotadavid <davidvillota44@gmail.com>
2021-06-13 13:51:59 +0200

    Linear scaling factor to 0.6

--- Codigos/1_Simplified A3C/A3C_utils.py
+++ Codigos/1_Simplified A3C/A3C_utils.py
@@ -138,7 +138,7 @@ def isDone(reward,collision):
 
 def interpret_action(action):
 
-    linear_scaling_factor = 1
+    linear_scaling_factor = 0.6
     if action == 0:
         quad_offset = (0, 0, +linear_scaling_factor)
     elif action == 1:
